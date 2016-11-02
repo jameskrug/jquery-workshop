ee30d0b6f4174eaf87dcd37851763d5a5617b292
James <krugger322@hotmail.com>
2016-11-02 19:48:07 +0000

    created exercise 2 files

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 /* Write your code here! */
+
+$(".kittylist").children("li").children("img").on("click", function(e){
+    var catSays = $(this).attr("alt");
+    var catUrl = $(this).attr("src")
+    // console.log("clicked this cat", catSays, catUrl);
+    $(".overlay").children("img").attr("src",catUrl);
+    $(".overlay").children(".image-caption").text(catSays);
+    // console.log("overlay",$(".overlay").attr("src"));
+    $(".overlay").addClass("displayoverlay");
+});
+
+$(".overlay").on("click",function(e){
+    $(".overlay").removeClass("displayoverlay");
+});
