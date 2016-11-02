27feef47ed621eebd6a37a585e1e04c0119c046b
James <krugger322@hotmail.com>
2016-11-02 21:35:32 +0000

    added files for exercise 3a and 3b

--- script/app.js
+++ script/app.js
@@ -1,1 +1,30 @@
-/* Write your code here! */
+var mq = window.matchMedia( "(min-width: 641px)" );
+
+function WidthChange(mq) {
+  if (mq.matches) {
+    $(document).ready(function(){
+        $(".divform").sticky({topSpacing:0});
+    });
+  } 
+  else{
+      $(document).ready(function(){
+        $(".divform").unstick();
+    });
+  }
+}
+
+
+
+
+if (matchMedia) {
+  window.matchMedia("(min-width: 641px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+
+if (mq.matches){
+    $(document).ready(function(){
+        $(".divform").sticky({topSpacing:0});
+    });
+}
