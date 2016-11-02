d66f340299ca1b96286a32cdfe9e52800ea2bf71
James <krugger322@hotmail.com>
2016-11-02 18:20:11 +0000

    exercise 1 files

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
 /* Write your code here! */
+var $announcement = $(".announcement");
+
+$announcement.append('<input type = "button" class = "closebutton" value = "X"><input type="button" class = "showButton" value = "show announcement">');
+
+$(".closebutton").on("click", function(e){
+   $(this).parent().addClass("hide");
+});
+
+$(".showButton").on("click", function(e){
+    $(this).parent().removeClass("hide");
+})
