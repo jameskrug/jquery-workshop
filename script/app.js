10fdca33a45402924c451ccec23ba12bc8e6e322
James <krugger322@hotmail.com>
2016-11-02 23:14:38 +0000

    added exercise 5 files

--- script/app.js
+++ script/app.js
@@ -1,1 +1,9 @@
-/* Write your code here! */
+
+//This is not the ISS location, but the api was down at the time of coding so couldn't test properly
+jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=sydney').then(
+    function(res) {
+        $("h1").after('<div class = "location">this is where the iss is: ' + res.results[0].geometry.location.lat + " : " + res.results[0].geometry.location.lng + "</div>")
+        $(".location").after("<div class = 'whynotiss'>this is actually the location of syndey, the iss api was down at the time of coding</div>")
+        console.log(res.results[0].geometry.location);
+    }
+);
