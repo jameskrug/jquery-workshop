d2b019fcc517718cea117b9790aa1c3b4ebe0f01
James <krugger322@hotmail.com>
2016-11-02 21:58:15 +0000

    added files for exercise4

--- script/app.js
+++ script/app.js
@@ -1,1 +1,17 @@
-/* Write your code here! */
+
+$('#signup-form').on('submit', function(event) {
+    // event.preventDefault();
+    var username = $("#user").val();
+    var password = $("#password").val();
+    var password2 = $("#password2").val();
+    if (password.length < 10){
+        event.preventDefault();
+        alert("password too short")
+    }
+    if (password != password2){
+        event.preventDefault();
+        alert("passwords don't match");
+    }
+    console.log(event);
+    console.log(username.length,password.length,password2.length);
+});
